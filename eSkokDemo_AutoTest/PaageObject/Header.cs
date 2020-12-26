961600fe0dd17e117130cb537002d6db344c6ce8
byrkalukasz <lukasz.byrka1@gmail.com>
2020-12-26 23:25:51 +0100

    aktualizacja

--- eSkokDemo_AutoTest/PaageObject/Header.cs
+++ eSkokDemo_AutoTest/PaageObject/Header.cs
@@ -11,17 +11,17 @@ namespace Selenium.IDE
         }
         [FindsBy(How = How.XPath, Using = "/html/body/div/header/div[2]/nav/div/ul/li[1]/a")]
         public IWebElement Introdustio { get; set; }
-        [FindsBy(How = How.XPath, Using = "/html/body/div/header/div[2]/nav/div/ul/li[1]/a")]
+        [FindsBy(How = How.XPath, Using = "/html/body/div/header/div[2]/nav/div/ul/li[2]/a")]
         public IWebElement Selectrs { get; set; }
-        [FindsBy(How = How.XPath, Using = "/html/body/div/header/div[2]/nav/div/ul/li[1]/a")]
+        [FindsBy(How = How.XPath, Using = "/html/body/div/header/div[2]/nav/div/ul/li[3]/a")]
         public IWebElement SpecialElements { get; set; }
-        [FindsBy(How = How.XPath, Using = "/html/body/div/header/div[2]/nav/div/ul/li[1]/a")]
+        [FindsBy(How = How.XPath, Using = "/html/body/div/header/div[2]/nav/div/ul/li[4]/a")]
         public IWebElement TestCases { get; set; }
-        [FindsBy(How = How.XPath, Using = "/html/body/div/header/div[2]/nav/div/ul/li[1]/a")]
+        [FindsBy(How = How.XPath, Using = "/html/body/div/header/div[2]/nav/div/ul/li[5]/a")]
         public IWebElement TestScenario { get; set; }
-        [FindsBy(How = How.XPath, Using = "/html/body/div/header/div[2]/nav/div/ul/li[1]/a")]
-        public IWebElement Introdustio { get; set; }
-        [FindsBy(How = How.XPath, Using = "/html/body/div/header/div[2]/nav/div/ul/li[1]/a")]
-        public IWebElement Introdustio { get; set; }
+        [FindsBy(How = How.XPath, Using = "/html/body/div/header/div[2]/nav/div/ul/li[6]/a")]
+        public IWebElement TestScenarios { get; set; }
+        [FindsBy(How = How.XPath, Using = "/html/body/div/header/div[2]/nav/div/ul/li[7]/a")]
+        public IWebElement About { get; set; }
     }
 }
---
+++ eSkokDemo_AutoTest/RecentPost.cs
@@ -0,0 +1,24 @@
+﻿using OpenQA.Selenium;
+using SeleniumExtras.PageObjects;
+
+
+namespace Selenium.IDE
+{
+    class RecentPost
+    {
+        public RecentPost()
+        {
+            PageFactory.InitElements(Driver.ChromeDriver, this);
+        }
+        [FindsBy(How = How.XPath, Using = "/html/body/div/div[2]/aside/div[2]/ul/li[1]/a")]
+        public IWebElement DragAndDrop { get; set; }
+        [FindsBy(How = How.XPath, Using = "/html/body/div/div[2]/aside/div[2]/ul/li[2]/a")]
+        public IWebElement NewTab { get; set; }
+        [FindsBy(How = How.XPath, Using = "/html/body/div/div[2]/aside/div[2]/ul/li[3]/a")]
+        public IWebElement AlertBox { get; set; }
+        [FindsBy(How = How.XPath, Using = "/html/body/div/div[2]/aside/div[2]/ul/li[4]/a")]
+        public IWebElement Name { get; set; }
+        [FindsBy(How = How.XPath, Using = "/html/body/div/div[2]/aside/div[2]/ul/li[5]/a")]
+        public IWebElement ID { get; set; }
+    }
+}
